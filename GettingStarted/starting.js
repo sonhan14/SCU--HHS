26bb429abd40d687ac0cdf9ba5f1ef3d6b2b43a8
sonhan14 <sonhan14@gmail.com>
2021-11-23 09:51:52 +0700

    demo getting started

--- GettingStarted/starting.js
+++ GettingStarted/starting.js
@@ -5,6 +5,10 @@ const baz = () => console.log('baz')
 const foo = () => {
   console.log('foo')
   setTimeout(bar, 0)
+  new Promise((resolve, reject) =>{
+    resolve('Success execution!')
+  }).then(resolve => console.log(resolve))
+  .catch(error => console.log('Sorry!' + error))
   baz()
 }
 
